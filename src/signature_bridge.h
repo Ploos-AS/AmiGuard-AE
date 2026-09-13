5816ce051ac22139c8b60f0c116c23e592bd6c6d
pos <pgousdal@users.noreply.github.com>
2026-09-14 01:13:03 +0200

    M3.3: add signature update provider foundation

--- src/signature_bridge.h
+++ src/signature_bridge.h
@@ -3,6 +3,7 @@
 
 #define AMIGUARD_AE_SIGNATURE_TYPE_MAX 16
 #define AMIGUARD_AE_SIGNATURE_NAME_MAX 96
+#define AMIGUARD_AE_SIGNATURE_UPDATE_DETAIL_MAX 128
 
 typedef struct AmiGuardAESignatureInfo {
     char type[AMIGUARD_AE_SIGNATURE_TYPE_MAX];
@@ -15,11 +16,16 @@ typedef struct AmiGuardAESignatureInfo {
 typedef unsigned long (*AmiGuardAESignatureCountProvider)(void);
 typedef int (*AmiGuardAESignatureInfoProvider)(unsigned long index,
                                                AmiGuardAESignatureInfo *out);
+typedef int (*AmiGuardAESignatureUpdateProvider)(char *detail,
+                                                 unsigned long detail_size);
 
 void amiguard_ae_signature_set_count_provider(AmiGuardAESignatureCountProvider provider);
 void amiguard_ae_signature_set_info_provider(AmiGuardAESignatureInfoProvider provider);
+void amiguard_ae_signature_set_update_provider(AmiGuardAESignatureUpdateProvider provider);
 int amiguard_ae_signature_available(void);
+int amiguard_ae_signature_update_available(void);
 unsigned long amiguard_ae_signature_count(void);
 int amiguard_ae_signature_info(unsigned long index, AmiGuardAESignatureInfo *out);
+int amiguard_ae_signature_update(char *detail, unsigned long detail_size);
 
 #endif

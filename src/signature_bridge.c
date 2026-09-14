9f5f9f7acf670f94f18ef60a0ac589affb48dfb1
pos <pgousdal@users.noreply.github.com>
2026-09-14 05:15:25 +0200

    M3.5: connect AmiGuard runtime signature loader

--- src/signature_bridge.c
+++ src/signature_bridge.c
@@ -12,8 +12,10 @@ static AmiGuardAESignatureUpdateProvider update_provider = 0;
 
 static void copy_text(char *dst, unsigned long size, const char *src)
 {
-    if (size == 0UL) return;
-    if (src == 0) src = "";
+    if (size == 0UL)
+        return;
+    if (src == 0)
+        src = "";
     strncpy(dst, src, (size_t)(size - 1UL));
     dst[size - 1UL] = '\0';
 }
@@ -32,7 +34,9 @@ static int builtin_signature_info(unsigned long index, AmiGuardAESignatureInfo *
     unsigned long boot_count = 0UL;
     unsigned long file_count = 0UL;
 
-    if (out == 0) return 0;
+    if (out == 0)
+        return 0;
+
     boot = amiguard_signatures(&boot_count);
     if (index < boot_count) {
         copy_text(out->type, AMIGUARD_AE_SIGNATURE_TYPE_MAX, "BOOTBLOCK");
@@ -45,13 +49,22 @@ static int builtin_signature_info(unsigned long index, AmiGuardAESignatureInfo *
 
     file = amiguard_file_signatures(&file_count);
     index -= boot_count;
-    if (index >= file_count) return 0;
+    if (index >= file_count)
+        return 0;
+
     copy_text(out->type, AMIGUARD_AE_SIGNATURE_TYPE_MAX, "FILE");
     copy_text(out->name, AMIGUARD_AE_SIGNATURE_NAME_MAX, file[index].name);
     out->offset = file[index].offset;
     out->length = (unsigned long)file[index].length;
     out->test_only = file[index].test_only;
     return 1;
+}
+
+static int builtin_signature_update(const char *path,
+                                    char *detail,
+                                    unsigned long detail_size)
+{
+    return amiguard_file_signature_load_database(path, detail, detail_size);
 }
 #endif
 
@@ -72,7 +85,8 @@ void amiguard_ae_signature_set_update_provider(AmiGuardAESignatureUpdateProvider
 
 int amiguard_ae_signature_available(void)
 {
-    if (count_provider != 0) return 1;
+    if (count_provider != 0)
+        return 1;
 #if defined(AMIGUARD_AE_WITH_AMIGUARD)
     return 1;
 #else
@@ -82,12 +96,19 @@ int amiguard_ae_signature_available(void)
 
 int amiguard_ae_signature_update_available(void)
 {
-    return update_provider != 0;
+    if (update_provider != 0)
+        return 1;
+#if defined(AMIGUARD_AE_WITH_AMIGUARD)
+    return 1;
+#else
+    return 0;
+#endif
 }
 
 unsigned long amiguard_ae_signature_count(void)
 {
-    if (count_provider != 0) return count_provider();
+    if (count_provider != 0)
+        return count_provider();
 #if defined(AMIGUARD_AE_WITH_AMIGUARD)
     return builtin_signature_count();
 #else
@@ -97,7 +118,8 @@ unsigned long amiguard_ae_signature_count(void)
 
 int amiguard_ae_signature_info(unsigned long index, AmiGuardAESignatureInfo *out)
 {
-    if (info_provider != 0) return info_provider(index, out);
+    if (info_provider != 0)
+        return info_provider(index, out);
 #if defined(AMIGUARD_AE_WITH_AMIGUARD)
     return builtin_signature_info(index, out);
 #else
@@ -107,9 +129,19 @@ int amiguard_ae_signature_info(unsigned long index, AmiGuardAESignatureInfo *out
 #endif
 }
 
-int amiguard_ae_signature_update(char *detail, unsigned long detail_size)
+int amiguard_ae_signature_update(const char *path,
+                                 char *detail,
+                                 unsigned long detail_size)
 {
-    if (detail != 0 && detail_size != 0UL) detail[0] = '\0';
-    if (update_provider == 0) return 0;
-    return update_provider(detail, detail_size);
+    if (detail != 0 && detail_size != 0UL)
+        detail[0] = '\0';
+
+    if (update_provider != 0)
+        return update_provider(path, detail, detail_size);
+#if defined(AMIGUARD_AE_WITH_AMIGUARD)
+    return builtin_signature_update(path, detail, detail_size);
+#else
+    (void)path;
+    return 0;
+#endif
 }
